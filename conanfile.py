967013fd915aa301a463cc00c083d1f906f39c23
Dean Moldovan <dean0x7d@gmail.com>
2024-04-29 11:34:21 +0200

    Update recipe with Conan v2 support

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,13 @@
 import os
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-from conans.tools import Version
+import pathlib
+from conan import ConanFile
+from conan.tools import files
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
 
 class CAFConan(ConanFile):
     name = "caf"
-    version = "0.17.4-lmx.13"
+    version = "0.17.4-lmx.14"
     description = "An open source implementation of the Actor Model in C++"
     url = "https://github.com/bincrafters/conan-caf"
     homepage = "https://github.com/actor-framework/actor-framework"
@@ -14,7 +15,7 @@ class CAFConan(ConanFile):
     license = ("BSD-3-Clause, BSL-1.0")
     exports = ["LICENSE"]
     exports_sources = ["CMakeLists.txt", "cmake/*", "doc/*", "libcaf*"]
-    generators = ["cmake"]
+    generators = "CMakeDeps"
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
@@ -31,11 +32,14 @@ class CAFConan(ConanFile):
 
     @property
     def _is_static(self):
-        return 'shared' not in self.options.values.keys() or not self.options.shared
+        return 'shared' not in self.options or not self.options.shared
 
     @property
     def _has_openssl(self):
-        return 'openssl' in self.options.values.keys() and self.options.openssl
+        return 'openssl' in self.options and self.options.openssl
+
+    def layout(self):
+        cmake_layout(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -48,57 +52,42 @@ class CAFConan(ConanFile):
         if self._has_openssl:
             self.requires("openssl/1.0.2u")
 
-    def configure(self):
-        if self.settings.compiler == "gcc":
-            if Version(self.settings.compiler.version.value) < "4.8":
-                raise ConanInvalidConfiguration("g++ >= 4.8 is required, yours is %s" % self.settings.compiler.version)
-        elif self.settings.compiler == "clang" and Version(self.settings.compiler.version.value) < "4.0":
-            raise ConanInvalidConfiguration("clang >= 4.0 is required, yours is %s" % self.settings.compiler.version)
-        elif self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version.value) < "9.0":
-            raise ConanInvalidConfiguration("clang >= 9.0 is required, yours is %s" % self.settings.compiler.version)
-        elif self.settings.compiler == "apple-clang" and Version(self.settings.compiler.version.value) > "10.0" and \
-                self.settings.arch == 'x86':
-            raise ConanInvalidConfiguration("clang >= 11.0 does not support x86")
-        elif self.settings.compiler == "Visual Studio" and Version(self.settings.compiler.version.value) < "15":
-            raise ConanInvalidConfiguration("Visual Studio >= 15 is required, yours is %s" % self.settings.compiler.version)
-
-    def _cmake_configure(self):
-        cmake = CMake(self)
-        cmake.definitions["CMAKE_CXX_STANDARD"] = self.settings.compiler.cppstd
-        cmake.definitions["CAF_NO_AUTO_LIBCPP"] = True
-        cmake.definitions["CAF_NO_OPENSSL"] = not self._has_openssl
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CAF_NO_AUTO_LIBCPP"] = True
+        tc.variables["CAF_NO_OPENSSL"] = not self._has_openssl
         for define in ["CAF_NO_EXAMPLES", "CAF_NO_TOOLS", "CAF_NO_UNIT_TESTS", "CAF_NO_PYTHON", "CAF_NO_OPENCL"]:
-            cmake.definitions[define] = "ON"
-        if tools.os_info.is_macos and self.settings.arch == "x86":
-            cmake.definitions["CMAKE_OSX_ARCHITECTURES"] = "i386"
-        cmake.definitions["CAF_BUILD_STATIC"] = self._is_static
-        cmake.definitions["CAF_BUILD_STATIC_ONLY"] = self._is_static
-        cmake.definitions["CAF_LOG_LEVEL"] = self.options.log_level
+            tc.variables[define] = "ON"
+        if self.settings.os == "Macos" and self.settings.arch == "x86":
+            tc.variables["CMAKE_OSX_ARCHITECTURES"] = "i386"
+        tc.variables["CAF_BUILD_STATIC"] = self._is_static
+        tc.variables["CAF_BUILD_STATIC_ONLY"] = self._is_static
+        tc.variables["CAF_LOG_LEVEL"] = self.options.log_level
         if self.settings.os == 'Windows':
-            cmake.definitions["OPENSSL_USE_STATIC_LIBS"] = True
-            cmake.definitions["OPENSSL_MSVC_STATIC_RT"] = True
+            tc.variables["OPENSSL_USE_STATIC_LIBS"] = True
+            tc.variables["OPENSSL_MSVC_STATIC_RT"] = True
         elif self.settings.compiler == 'clang':
-            cmake.definitions["PTHREAD_LIBRARIES"] = "-pthread -ldl"
+            tc.variables["PTHREAD_LIBRARIES"] = "-pthread -ldl"
         else:
-            cmake.definitions["PTHREAD_LIBRARIES"] = "-pthread"
-            if self.settings.compiler == "gcc" and Version(self.settings.compiler.version.value) < "5.0":
-                cmake.definitions["CMAKE_SHARED_LINKER_FLAGS"] = "-pthread"
-        cmake.definitions["CAF_NO_IO"] = not self.options.io_module
-        cmake.definitions["CAF_ALWAYS_ALLOW_UNSAFE"] = self.options.always_allow_unsafe
-        cmake.configure(build_folder=self._build_subfolder)
-        return cmake
+            tc.variables["PTHREAD_LIBRARIES"] = "-pthread"
+        tc.variables["CAF_NO_IO"] = not self.options.io_module
+        tc.variables["CAF_ALWAYS_ALLOW_UNSAFE"] = self.options.always_allow_unsafe
+        tc.generate()
 
     def build(self):
-        cmake = self._cmake_configure()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE*", dst="licenses")
-        cmake = self._cmake_configure()
+        pck_folder = pathlib.Path(self.package_folder)
+        files.copy(self, pattern="LICENSE*", src=self.source_folder, dst=pck_folder / "licenses")
+        cmake = CMake(self)
+        cmake.configure()
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = files.collect_libs(self)
         if self.settings.os == "Windows":
             self.cpp_info.system_libs.extend(["ws2_32", "iphlpapi", "psapi"])
         elif self.settings.os == "Linux":
